2ae6a0fabf6d8797c043e854c5740242c8c62d83
sujji <83086675+psg759@users.noreply.github.com>
2023-10-12 19:41:13 +0900

    [Silver II] Title: 촌수계산, Time: 136 ms, Memory: 112004 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,19 +1,15 @@
 from collections import deque
-# 2644번 촌수 계산
-# dfs로 풀 경우 내가 찾는 사람을 발견했다 하더라도, 나머지 펼쳐진 재귀들을 다 수습을 해야하는 문제때문에 bfs 로 풀어야할듯
-# bfs로 해봤는데 이걸로 하면 3까지 최단거리로 가는게 아니라 가까운 애들을 거쳐서 먼저 가기때문에 dfs가 적합함 수정방안을 생각해보자 ..
-# 정답을 보고 내 식대로 다시 bfs로 해결해보자,,
 
-#전체 사람 수
 n = int(input())
 a,b = map(int, input().split())
 m = int(input())
 
 graph = []
-for i in range(m):
+
+for _ in range(m):
     graph.append(list(map(int, input().split())))
-    
-data = [ [] for _ in range(n+1)]
+
+data = [[] for _ in range(n+1)]
 
 for i in graph:
     data[i[0]].append(i[1])
@@ -21,24 +17,24 @@ for i in graph:
     
 for j in data:
     j.sort()
-    
-def bfs(graph, start, visited):
-    queue = deque([start])
-    visited[start] = 0
+
+visited = [0] * (n+1)
+
+def bfs(a,b):
+    queue = deque([a])
+    visited[a] = 0
     
     while queue:
         v = queue.popleft()
         
         if v == b:
             return visited[b]
-            
-        for i in graph[v]:
-            if visited[i] == 0:
-                queue.append(i)
-                visited[i] = visited[v] + 1
-                
+        
+        for k in data[v]:
+            if visited[k] == 0:
+                queue.append(k)
+                visited[k] = visited[v] + 1
+    
     return -1
-         
-visited = [0] * (n+1)
 
-print(bfs(data,a,visited))
+print(bfs(a,b))
