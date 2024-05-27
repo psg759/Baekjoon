bb24b4cc8944239c9257a6c02018c6fb972b0b78
sujji <83086675+psg759@users.noreply.github.com>
2024-05-27 21:08:14 +0900

    [level 2] Title: 교점에 별 만들기, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.py
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.py
@@ -1,40 +1,39 @@
 def solution(line):
     answer = []
-
-    # 교점 계산
+    real_ans = []
     for i in range(len(line)):
-        for j in range(i + 1, len(line)):
-            a1, b1, c1 = line[i]
-            a2, b2, c2 = line[j]
+        for j in range(len(line) - 1 - i):
+            a1, b1, c1 = line[i][0], line[i][1], line[i][2]
+            a2, b2, c2 = line[i+j+1][0], line[i+j+1][1], line[i+j+1][2]
             cross_dot = a1 * b2 - b1 * a2
 
-            # 평행 상태이므로 교점 없음
+            #평행 상태이므로 교점 x
             if cross_dot == 0:
                 continue
-
-            # 교점이 존재
-            x = ((b1 * c2) - (c1 * b2)) / cross_dot
-            y = ((c1 * a2) - (a1 * c2)) / cross_dot
-
-            # 교점 x, y가 정수라면 answer에 추가
-            if x == int(x) and y == int(y):
-                point = [int(x), int(y)]
-                if point not in answer:
-                    answer.append(point)
-
-    if not answer:
+            #교점이 존재
+            else:
+                x = ((b1 * c2) - (c1 * b2)) / cross_dot
+                y = ((c1 * a2) - (a1 * c2)) / cross_dot
+                #교점 x, y가 정수라면 answer에 추가
+                if x == int(x) and y == int(y) and [int(x), int(y)] not in answer:
+                    answer.append([int(x),int(y)])
+    #교점이 한개라면
+    if len(answer) == 1:
         return ["*"]
+    
 
-    # 교점의 x, y 좌표 최솟값과 최댓값을 구하기
-    x_min = min(x for x, y in answer)
-    x_max = max(x for x, y in answer)
-    y_min = min(y for x, y in answer)
-    y_max = max(y for x, y in answer)
-
+    # 교점이 한개가 아닐때의 사각형 너비 구하기
+    answer = sorted(answer, key = lambda x : x[0])
+    x_min, x_max = answer[0][0], answer[-1][0]
     width = x_max - x_min + 1
+    
+    # 교점이 한개가 아닐때의 사각형 높이 구하기 
+    answer = sorted(answer, key = lambda x : x[1])
+    y_min, y_max = answer[0][1], answer[-1][1]
     height = y_max - y_min + 1
 
-    # 좌표를 (0, 0) 기준으로 이동
+
+   # 좌표를 (0, 0) 기준으로 이동
     normalized_points = [(x - x_min, y_max - y) for x, y in answer]
 
     # 사각형 그리기
@@ -45,4 +44,3 @@ def solution(line):
     real_ans = [''.join(row) for row in canvas]
     
     return real_ans
-
