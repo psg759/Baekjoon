d84745f8857a1531f0c4f032fe15a89c1fcafffe
sujji <83086675+psg759@users.noreply.github.com>
2024-10-14 23:45:04 +0900

    [Silver V] Title: 폴리오미노, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.py
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -1,36 +1,27 @@
-board = input()
-tmp = ''
+pol = input()
+p = len(pol)
 cnt = 0
+res = ""
 
-for i in range(len(board)):
-    if board[i] == "X":
+for i in range(p):
+    if pol[i] == "X":
         cnt += 1
-    elif board[i] == '.':
-        if cnt % 2 == 1:
+    elif pol[i] == ".":
+        if cnt % 4 == 0:
+            res += "A" * cnt + "."
+        elif cnt % 2 == 0:
+            res += "A" * (cnt-2) + "B" * 2 + "."
+        else:
             print(-1)
             exit()
-        else:
-            if cnt == 2:
-                tmp += 'BB'
-            elif cnt == 4:
-                tmp += 'AAAA'
-            else:
-                a = cnt % 4
-                cnt = cnt // 4
-                tmp += 'AAAA' * cnt + 'BB' * (a//2)
-        tmp += '.'
         cnt = 0
-    if i == (len(board) - 1):
-        if cnt % 2 == 1:
+    if i == p - 1:
+        if cnt % 4 == 0:
+            res += "A" * cnt
+        elif cnt % 2 == 0:
+            res += "A" * (cnt-2) + "B" * 2 
+        else:
             print(-1)
             exit()
-        else:
-            if cnt == 2:
-                tmp += 'BB'
-            elif cnt == 4:
-                tmp += 'AAAA'
-            else:
-                a = cnt % 4
-                cnt = cnt // 4
-                tmp += 'AAAA' * cnt + 'BB' * (a//2)
-print(tmp)
+
+print(res)
